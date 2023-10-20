b3560a4ff2f2e6332763ddf54373caf52f54f421
Mohammed Rifatul Islam <mdrifatulislam08@gmail.com>
2023-10-21 00:52:06 +0600

    update cart

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -28,8 +28,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const CarCollection = client.db("DriverBD").collection("cars") 
+    const cartCollection = client.db("DriverBD").collection('carts')
 
     // read data
     app.get('/cars', async(req, res)=>{
@@ -38,18 +39,74 @@ async function run() {
       res.send(result)
     })
     
-
     // create data
     app.post('/cars',async (req, res)=>{
       const newCars = req.body
-      console.log(newCars);
-      const result = await CarCollection.insertOne(newCars)
+      const result = await CarCollection.insertMany(newCars)
       res.send(result)
     })
 
+     // get data by id
+     app.get('/cars/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await CarCollection.findOne(query)
+      res.send(result)
+    })
+
+    //add cart data
+    app.post('/carts', async(req, res) =>{
+      const addCart = req.body
+      const result = await cartCollection.insertOne(addCart)
+      res.send(result)
+    })
+    //read add cart
+    app.get('/carts', async(req, res) =>{
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/carts/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: id}
+      const result = await cartCollection.findOne(query)
+      res.send(result)
+    })
+
+    // delete from add cart
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: id}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+ 
+
+       
+    // update data
+    app.put('/cars/:id', async(req, res) =>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateCar = req.body;
+      const car = {
+        $set: {
+          name:updateCar.name,
+          brand_name:updateCar.brand_name,
+          type:updateCar.type, 
+          price:updateCar.price,
+          description:updateCar.description,
+          photo:updateCar.photo,
+          rating:updateCar.rating
+        },
+      }
+      const result = await CarCollection.updateOne(filter,car,options)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
